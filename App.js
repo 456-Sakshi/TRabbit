80fe8ad1afa4c5c97445806df2d1cd2078ffdd90
456-Sakshi <108233971+456-Sakshi@users.noreply.github.com>
2024-10-14 14:26:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UserForm from './components/UserForm.js';
+import NavBar from './components/NavBar.js';
+
+
+function App() {
+  return (
+    <div>
+    <NavBar/>
+    <UserForm/>
+    </div>
+  );
+}
+
+export default App;
